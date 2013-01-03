24fa70ade78bedfb6589b47b319d29a5cf056c8b
tetsus <daniel@levelsetlabs.com>
2013-01-03 14:53:45 -0500

    moved gen_docs into grunt.js, now we can just do "grunt docs"

--- gen_docs.sh
+++
@@ -1,1 +0,0 @@
-cat src/*.js src/*/*.js | dox | node support/docs.js > docs/index.html
--- grunt.js
+++ grunt.js
@@ -1,3 +1,5 @@
+var exec = require('child_process').exec
+
 module.exports = function(grunt) {
 
     grunt.initConfig({
@@ -18,5 +20,23 @@ module.exports = function(grunt) {
     });
 
     grunt.registerTask('default', 'concat min');
+
+    grunt.registerTask('docs', 'Generate documentation using dox', function() {
+        var cb = this.async()
+        var child = exec("cat src/*.js src/*/*.js | dox | node support/docs.js > docs/index.html", 
+            function(err, stdout, stderr) {
+                if(stdout) {
+                    console.log('stdout: ', stdout)
+                }
+                if(stderr) {
+                    console.log('stderr: ', stderr)
+                }
+                if(err !== null) {
+                    console.log('exec error: ', err)
+                }
+                cb()
+            }
+        )  
+    });
 
 }
